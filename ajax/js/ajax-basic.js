367c929205839e08d1bf726e77fd9db12d2d7664
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-11-15 23:47:31 +0600

    add: start learning ajax

---
+++ ajax/js/ajax-basic.js
@@ -0,0 +1,10 @@
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById("demo").innerHTML = this.responseText;
+  };
+  xhttp.open("GET", "ajax_info.txt");
+  xhttp.send();
+}
+
+console.log("Hellow Wrold");
