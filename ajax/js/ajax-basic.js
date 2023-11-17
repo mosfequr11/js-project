53a86a2795865e5c25305907ff3a4eaed3e115e4
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-11-17 19:04:19 +0600

    fix: create a text file and call it

--- ajax/js/ajax-basic.js
+++ ajax/js/ajax-basic.js
@@ -1,10 +1,9 @@
 function loadDoc() {
+  //The XMLHttpRequest Object
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
-    document.getElementById("demo").innerHTML = this.responseText;
+    document.getElementById("container").innerHTML = this.responseText;
   };
-  xhttp.open("GET", "ajax_info.txt");
+  xhttp.open("GET", "http://127.0.0.1:5500/ajax/file/change-text.txt");
   xhttp.send();
 }
-
-console.log("Hellow Wrold");
