1af92347ac0806a4c1160ee8cc227eb24cfdfd40
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-11-18 23:48:33 +0600

    add: count vowel in a sentense

--- problem-solving/js/problem-solving.js
+++ problem-solving/js/problem-solving.js
@@ -1,6 +1,20 @@
 //find the duplicate number from an array
-const number = [1, 2, 5, 7, 7, 2, 9, 5, 4, 3];
-const duplicate = number.filter(function (po, index, array) {
-  return array.indexOf(po) !== index;
-});
-console.log(duplicate);
+// const number = [1, 2, 5, 7, 7, 2, 9, 5, 4, 3];
+// const duplicate = number.filter(function (po, index, array) {
+//   return array.indexOf(po) !== index;
+// });
+// console.log(duplicate);
+
+//count vowel in a sentense
+const vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+function countVowel(sentense) {
+  let count = 0;
+  const letters = Array.from(sentense);
+  letters.forEach(function (value) {
+    if (vowel.includes(value)) {
+      count++;
+    }
+  });
+  return count;
+}
+console.log(countVowel("Aeiou Aeiou Ae"));
