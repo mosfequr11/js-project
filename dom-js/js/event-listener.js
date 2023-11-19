8ca820eb6258500a5af4b688efa0150e0afbd318
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-11-19 13:09:20 +0600

    add: Many Event Handlers to the Same Element

---
+++ dom-js/js/event-listener.js
@@ -0,0 +1,27 @@
+// element.addEventListener(event, function, useCapture);
+// The first parameter is the type of the event (like "click" or "mousedown" or any other HTML DOM Event.)
+// The second parameter is the function we want to call when the event occurs.
+// The third parameter is a boolean value specifying whether to use event bubbling or event capturing. This parameter is optional.
+
+// Add an Event Handler to an Element
+document.getElementById("buttonout").addEventListener("click", function () {
+  alert("Hello World!");
+});
+
+// Add Many Event Handlers to the Same Element
+function manyEventHandlers() {
+  document.getElementById("output").innerHTML += "Moused over!<br>";
+}
+
+function manyEventHandlersClick() {
+  document.getElementById("output").innerHTML += "Clicked!<br>";
+}
+
+function manyEventHandlersOut() {
+  document.getElementById("output").innerHTML += "Moused out!<br>";
+}
+
+var buttonin = document.getElementById("buttonin");
+buttonin.addEventListener("mouseover", manyEventHandlers);
+buttonin.addEventListener("click", manyEventHandlersClick);
+buttonin.addEventListener("mouseout", manyEventHandlersOut);
