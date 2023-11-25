2e70b78f11f5176f1bd5d181585ae40b92bbb2ef
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-11-25 22:30:23 +0600

    fix: two ajax response function

--- ajax/js/response-ajax.js
+++ ajax/js/response-ajax.js
@@ -2,7 +2,11 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
+      document.getElementById("container").innerHTML = this.responseText;
+      // The getAllResponseHeaders() method returns all header information from the server response.
+      // console.log(this.getAllResponseHeaders());
+      //The getResponseHeader() method returns specific header information from the server response.
+      console.log(this.getResponseHeader("cache-control"));
     }
   };
   xhttp.open(
