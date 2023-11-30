9fd0c9f83d5ac3595a7875830b88e7f352162c74
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-11-30 23:55:22 +0600

    add: Got the data from the server as a string and an object

--- ajax/js/getdata-ajax.js
+++ ajax/js/getdata-ajax.js
@@ -5,11 +5,26 @@
 //     .then((json) => console.log(json));
 // }
 
+// Got the data from the server as string, not an object
+// function getDataFun() {
+//   //The XMLHttpRequest Object
+//   const xhttp = new XMLHttpRequest();
+//   xhttp.onload = function () {
+//     console.log(xhttp.responseText);
+//   };
+//   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+//   xhttp.send();
+// }
+
+// Got the data from the server as an object
 function getDataFun() {
   //The XMLHttpRequest Object
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
-    console.log(xhttp.responseText);
+    //I checked what kind of data I received from the server.
+    // console.log(typeof xhttp.responseText);
+    //converted the data string to valid javascript an object
+    console.log(JSON.parse(xhttp.responseText));
   };
   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
   xhttp.send();
