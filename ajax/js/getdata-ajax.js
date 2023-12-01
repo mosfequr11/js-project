566c689c43c0b7cbbaa0686a9d2fd24dad0a52d7
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-12-01 22:51:28 +0600

    add: converted string to JSON object

--- ajax/js/getdata-ajax.js
+++ ajax/js/getdata-ajax.js
@@ -17,17 +17,33 @@
 // }
 
 // Got the data from the server as an object
+// function getDataFun() {
+//   //The XMLHttpRequest Object
+//   const xhttp = new XMLHttpRequest();
+//   xhttp.onload = function () {
+//     //I checked what kind of data I received from the server.
+//     // console.log(typeof xhttp.responseText);
+//     //converted the data string to valid javascript an object
+//     console.log(JSON.parse(xhttp.responseText));
+//     //show data on the browser
+//     document.getElementById("show-data").innerHTML = xhttp.responseText;
+//   };
+//   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+//   xhttp.send();
+// }
+
+// Got the data from the server as a string then I covert it to JSON format in another way
 function getDataFun() {
   //The XMLHttpRequest Object
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
-    //I checked what kind of data I received from the server.
-    // console.log(typeof xhttp.responseText);
-    //converted the data string to valid javascript an object
-    console.log(JSON.parse(xhttp.responseText));
-    document.getElementById("show-data").innerHTML = xhttp.responseText;
+    // I checked the data in the console area
+    console.log(xhttp.response);
+    //show data on the browser
+    document.getElementById("show-data").innerHTML = xhttp.response;
   };
   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+  xhttp.responseType = "json";
   xhttp.send();
 }
 
