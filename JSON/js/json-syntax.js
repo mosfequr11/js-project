c6d75c5ce59c927077d2786f05a8681f05223e3c
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-12-04 00:10:56 +0600

    add: json syntax learning

---
+++ JSON/js/json-syntax.js
@@ -0,0 +1,7 @@
+// console.log("hellow");
+
+const myObj = { name: "Mosfequr", age: 30, city: "Dhaka" };
+console.log(myObj.name);
+myObj["name"] = "Rahman";
+console.log(myObj["name"]);
+// document.getElementById("showdata").innerHTML = myObj["name"];
