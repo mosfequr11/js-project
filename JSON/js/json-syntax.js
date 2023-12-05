23e18f41608d44008c3fc015a2488a2304140b9c
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-12-05 23:37:22 +0600

    add: display the data single to multiple lines

--- JSON/js/json-syntax.js
+++ JSON/js/json-syntax.js
@@ -10,18 +10,21 @@
 
 // this is a JSON string
 var info = '{"name":"Mosfequr", "age":30, "car": "BMW"}';
-console.log(info);
+// console.log(info);
 
 // JSON string convert to Object
 var infoObj = JSON.parse(info);
-console.log(infoObj);
-console.log(infoObj.car);
+// console.log(infoObj);
+// console.log(infoObj.car);
 // display the data with multiple line
-var displayObj = document.getElementsByClassName("str-to-obj");
-displayObj[0].innerHTML = infoObj.car;
+// var displayObj = document.getElementsByClassName("str-to-obj");
+// displayObj[0].innerHTML = infoObj.car;
+
+// display the data with single line
+document.getElementsByClassName("str-to-obj")[0].innerHTML = infoObj.age;
 
 // Object convert to JSON string
 var infoString = JSON.stringify(infoObj);
 console.log(infoString);
-// display the data
-document.getElementsByClassName("obj-to-str").innerHTML = infoString;
+// display the data with single line
+document.getElementsByClassName("obj-to-str")[0].innerHTML = infoString;
