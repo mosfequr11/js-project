7b7cf3835b3ec25ed446e2716663644059ed2f83
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-12-06 08:40:09 +0600

    add: different between JSON and Object

--- JSON/js/json-syntax.js
+++ JSON/js/json-syntax.js
@@ -1,30 +1,22 @@
-// console.log("hellow");
-
-// const myInfo = { name: "Mosfequr", age: 30, city: "Dhaka" };
-// //show value using object property
-// console.log(myInfo.name);
-// //replace the name property of object
-// myInfo["name"] = "Rahman";
-// console.log(myInfo["name"]);
-// document.getElementById("showdata").innerHTML = myInfo["name"];
+// this is an object
+// Data is in name/value pairs
+// Data is separated by commas
+// Curly braces hold objects
+// Square brackets hold arrays
+const Student = {
+  name: "Mosfequr",
+  age: 27,
+  skills: ["JavaScript", "Shopify", "WordPress"],
+};
+console.log(Student.skills);
 
 // this is a JSON string
-var info = '{"name":"Mosfequr", "age":30, "car": "BMW"}';
-// console.log(info);
-
-// JSON string convert to Object
-var infoObj = JSON.parse(info);
-// console.log(infoObj);
-// console.log(infoObj.car);
-// display the data with multiple line
-// var displayObj = document.getElementsByClassName("str-to-obj");
-// displayObj[0].innerHTML = infoObj.car;
-
-// display the data with single line
-document.getElementsByClassName("str-to-obj")[0].innerHTML = infoObj.age;
-
-// Object convert to JSON string
-var infoString = JSON.stringify(infoObj);
-console.log(infoString);
-// display the data with single line
-document.getElementsByClassName("obj-to-str")[0].innerHTML = infoString;
+// single-quote is not allowed in JSON name/value pair
+//JSON names require double quotes.
+// Data is in name/value pairs
+// Data is separated by commas
+// Curly braces hold objects
+// Square brackets hold arrays
+var studentInfo =
+  '{"name":"Mosfequr", "age":28, "car": "BMW" , "skills": ["JavaScript", "Shopify", "WordPress"]}';
+console.log(studentInfo);
---
+++ JSON/js/str-obj.js
@@ -0,0 +1,30 @@
+// console.log("hellow");
+
+// const myInfo = { name: "Mosfequr", age: 30, city: "Dhaka" };
+// //show value using object property
+// console.log(myInfo.name);
+// //replace the name property of object
+// myInfo["name"] = "Rahman";
+// console.log(myInfo["name"]);
+// document.getElementById("showdata").innerHTML = myInfo["name"];
+
+// this is a JSON string
+var info = '{"name":"Mosfequr", "age":30, "car": "BMW"}';
+// console.log(info);
+
+// JSON string convert to Object
+var infoObj = JSON.parse(info);
+console.log(infoObj);
+// console.log(infoObj.car);
+// display the data with multiple line
+// var displayObj = document.getElementsByClassName("str-to-obj");
+// displayObj[0].innerHTML = infoObj.car;
+
+// display the data with single line
+document.getElementsByClassName("str-to-obj")[0].innerHTML = infoObj.age;
+
+// Object convert to JSON string
+var infoString = JSON.stringify(infoObj);
+console.log(infoString);
+// display the data with single line
+document.getElementsByClassName("obj-to-str")[0].innerHTML = infoString;
