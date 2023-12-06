f8c65b26e1469130816ef1835d9a39bb28c1ed98
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-12-06 21:16:41 +0600

    add: arrays as JSON

--- JSON/js/json-syntax.js
+++ JSON/js/json-syntax.js
@@ -8,7 +8,7 @@ const Student = {
   age: 27,
   skills: ["JavaScript", "Shopify", "WordPress"],
 };
-console.log(Student.skills);
+// console.log(Student.skills);
 
 // this is a JSON string
 // single-quote is not allowed in JSON name/value pair
@@ -19,4 +19,21 @@ console.log(Student.skills);
 // Square brackets hold arrays
 var studentInfo =
   '{"name":"Mosfequr", "age":28, "car": "BMW" , "skills": ["JavaScript", "Shopify", "WordPress"]}';
-console.log(studentInfo);
+// console.log(studentInfo);
+
+// In JSON, values must be one of the following data types:
+// 1. a string
+// 2. a number
+// 3. an object
+// 4. an array
+// 5. a boolean
+// 6. null
+var dataTypes =
+  '{"name":"Mosfequr", "age":28, "car": "BMW" , "skills": ["JavaScript", "Shopify", "WordPress"],"reading":yes, game:null}';
+console.log(dataTypes);
+
+// JavaScript Arrays as JSON
+var arraysJson = [
+  '{"name":"Mosfequr", "age":28, "car": "BMW" , "skills": ["JavaScript", "Shopify", "WordPress"]}',
+];
+console.log(arraysJson);
