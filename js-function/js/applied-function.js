d3a70cad2408ad35bdca4b39750d4e19508c50a6
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-12-08 23:27:17 +0600

    add: Invoking a function

--- js-function/js/applied-function.js
+++ js-function/js/applied-function.js
@@ -8,5 +8,26 @@ function myInfo(arg1, arg12) {
 // console.log(myInfo(23, "Mirpur"));
 const x = new myInfo(23, "Mirpur");
 //  data showing as an object
-console.log(x);
-console.log(x.age);
+// console.log(x);
+// console.log(x.age);
+
+//
+// Invoking a JavaScript Function
+var D = 6;
+function f() {
+  console.log("I am a function");
+}
+// f();
+// console.log(D);
+function p() {
+  console.log(this);
+}
+// p();
+
+const j = {
+  a: function () {
+    console.log(this);
+    console.log("Mosfequr");
+  },
+};
+j.a();
