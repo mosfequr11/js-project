a2111fac108fc84288805a8592dab3555cc29be3
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-12-16 22:40:59 +0600

    add: learn invoking a function

--- js-function/js/various-function.js
+++ js-function/js/various-function.js
@@ -1,8 +1,31 @@
-//Show the last digit of the number
-function callNumber() {
-  var params = document.getElementById("myNum").value;
-  for (i = 0; i < params.length; i++) {
-    console.log(params[i]);
-    document.getElementById("showNumber").innerHTML = params[i];
-  }
+//Show the last digit of the number  //
+// function callNumber() {
+//   var params = document.getElementById("myNum").value;
+//   for (i = 0; i < params.length; i++) {
+//     console.log(params[i]);
+//     document.getElementById("showNumber").innerHTML = params[i];
+//   }
+// }
+
+// Invoking a JavaScript Function //
+// var a = 6;
+// console.log(a);
+function aInvoking() {
+  console.log("I am a function");
 }
+// it showing undefined
+// console.log(aInvoking());
+// Correct invoking the function
+aInvoking();
+
+function a() {
+  console.log(this);
+}
+a();
+
+const x = {
+  a: function () {
+    console.log(this);
+  },
+};
+x.a();
