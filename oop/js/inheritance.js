d847deb95dc8108c77cb1842166e0869051d63ac
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2023-12-26 22:10:12 +0600

    add: define a subclass from base class

--- oop/js/inheritance.js
+++ oop/js/inheritance.js
@@ -15,7 +15,28 @@ class Animal {
   }
 }
 
-const animal = new Animal("cat");
+const animal = new Animal("Animal");
 animal.eat();
+animal.sleep();
 
 // Inheritance is the process of creating a new Child class based on an existing Parent class.
+// define a subclass called Cat that inherits from Animal
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  // Child class new method meow()
+  meow() {
+    console.log(`${this.name} says meow.`);
+  }
+
+  // override the sleep() method from Animal base class
+  sleep() {
+    console.log(`${this.name} is sleeping on bed.`);
+  }
+}
+
+const objCat = new Cat("Cat");
+objCat.eat();
+objCat.sleep();
+objCat.meow();
