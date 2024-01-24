550450c8584f9cd1f80c728bf7b5a2a42165187c
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-01-24 23:52:35 +0600

    add: how to handle javascript synchronous blocking behavior

--- ajax/js/async-callback.js
+++ ajax/js/async-callback.js
@@ -1,11 +1,34 @@
-// check javascript synchronous blocking behavior
+// how to handle javascript synchronous blocking behavior
+/// start ///
+const takeOrder = (customer) => {
+  console.log(`take order for ${customer}`);
+};
 const processOrder = (customer) => {
-  console.log("processing order for customer 1");
-  var curentTime = new Date().getTime();
-  while (curentTime + 1000 >= new Date().getTime())
-    console.log(" order processing for customer 1");
+  console.log(`order processing for ${customer}`);
+  setTimeout(() => {
+    console.log(`cooking completed`);
+    console.log(`order processing for ${customer} is successful`);
+  }, 1000);
 };
 
-console.log("take order  from customer 1");
-// processOrder();
-console.log("completed order  for customer 1");
+const completeOrder = (customer) => {
+  console.log(`order complete for ${customer}`);
+};
+takeOrder("mosfequr");
+
+/// end ///
+
+// check javascript synchronous blocking behavior
+/// start ///
+// const processOrder = (customer) => {
+//   console.log("processing order for customer 1");
+//   var curentTime = new Date().getTime();
+//   while (curentTime + 1000 >= new Date().getTime())
+//     console.log(" order processing for customer is successful");
+// };
+
+// console.log("take order  from customer 1");
+// // processOrder();
+// console.log("completed order  for customer 1");
+
+/// end ///
