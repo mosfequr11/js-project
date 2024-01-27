11de4a8ed29968ef38515e7ccee2b2475e188725
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-01-27 23:49:55 +0600

    add: multiple promise concurrently

--- ajax/js/async-promises.js
+++ ajax/js/async-promises.js
@@ -1,25 +1,39 @@
-// start learning js promises in details (resolve,reject,then,catch)
+// multiple promise concurrently
 /// start ///
-const hasLearning = false;
-const learnPromise = new Promise((resolve, reject) => {
-  if (!hasLearning) {
-    const learningDetails = {
-      name: "Technical Meeting",
-      location: "Skype",
-      time: "8:pm",
-    };
-    resolve(learningDetails);
-  } else {
-    reject(new Error("Learing already scheduled"));
-  }
+const promiseConcurrently1 = Promise.resolve(`promise 1 resolved`);
+const promiseConcurrently2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(`promise 2 resolved`);
+  }, 1000);
 });
 
-learnPromise
-  .then((res) => {
-    console.log(JSON.stringify(res));
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+promiseConcurrently1.then((res) => console.log(res));
+promiseConcurrently2.then((res) => console.log(res));
+
+/// end ///
+
+// start learning js promises in details (resolve,reject,then,catch)
+/// start ///
+// const hasLearning = false;
+// const learnPromise = new Promise((resolve, reject) => {
+//   if (!hasLearning) {
+//     const learningDetails = {
+//       name: "Technical Meeting",
+//       location: "Skype",
+//       time: "8:pm",
+//     };
+//     resolve(learningDetails);
+//   } else {
+//     reject(new Error("Learing already scheduled"));
+//   }
+// });
+
+// learnPromise
+//   .then((res) => {
+//     console.log(JSON.stringify(res));
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
 
 /// end ///
