e99c5b46b304b6e84ce6203593b7a92b90c79fc0
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-02-02 23:18:39 +0600

    add: Sequence Control (callback)

--- js-function/js/callback-function.js
+++ js-function/js/callback-function.js
@@ -1,4 +1,21 @@
 /// start ///
+/// Sequence Control
+function displayNum(displayNum) {
+  document.getElementById("display-num-output").innerHTML = displayNum;
+}
+// here p and q are function parameter
+function sumNum(p, q) {
+  let sum = p + q;
+  return sum;
+}
+var j = 6;
+var k = 7;
+// here j and k is function argument
+let result = sumNum(j, k);
+displayNum(result);
+/// end ///
+
+/// start ///
 /// A callback is a function passed as an argument to another function. This technique allows a function to call another function. A callback function can run after another function has finished
 
 /// JavaScript functions are executed in the sequence they are called. Not in the sequence they are defined.
