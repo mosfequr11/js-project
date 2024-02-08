e873b9e136bb7d66710e50bd6b8233465bc1334e
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-02-08 23:58:43 +0600

    add: learn length (string) method

---
+++ string/js/string-methods.js
@@ -0,0 +1,8 @@
+/// The length property returns the length of a string
+var contentText = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(contentText.length);
+
+var countLength = document.getElementsByClassName("display-length");
+countLength.innerHTML = contentText.length;
+
+// document.getElementsByClassName("display-length").innerHTML = contentText.length;
