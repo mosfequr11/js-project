bb4aae11e6b5c0b70d14e40ef25d62ca9dbf35ef
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-02-09 23:49:34 +0600

    add: split() method learning

--- string/js/string-methods.js
+++ string/js/string-methods.js
@@ -1,8 +1,27 @@
+///  Start  ///
+/// A string can be converted to an array with the split() method:
+
+let contentTextTwo = "Hello";
+const arrSplit = contentTextTwo.split("");
+// console.log(arrSplit);
+
+loopItem = " ";
+for (let i = 0; i < arrSplit.length; i++) {
+  loopItem += arrSplit[i] + "<br>";
+  // console.log(loopItem);
+}
+console.log(loopItem);
+var countSplit = document.getElementsByClassName("display-split");
+countSplit.innerHTML = loopItem;
+
+///  End  ///
+///  Start  ///
 /// The length property returns the length of a string
 var contentText = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-console.log(contentText.length);
+// console.log(contentText.length);
 
 var countLength = document.getElementsByClassName("display-length");
 countLength.innerHTML = contentText.length;
 
 // document.getElementsByClassName("display-length").innerHTML = contentText.length;
+///  End  ///
