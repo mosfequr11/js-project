0e85559577a4d02c934b11d398b5fd2168c65d97
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-02-10 23:08:25 +0600

    add: learn concat() string method

--- string/js/string-methods.js
+++ string/js/string-methods.js
@@ -1,20 +1,32 @@
 ///  Start  ///
+/// concat() joins two or more strings:
+/// Note: All string methods return a new string. They don't modify the original string.Formally said: Strings are immutable: Strings cannot be changed, only replaced.
+let firstName = "Mosfequr";
+let lastName = "Rahman";
+// let myAge = "twenty Eight years old";
+let myAge = "28 years old ";
+let fullName = firstName.concat(" ", lastName).concat(" ", myAge);
+document.getElementById("displayConcat").innerHTML = fullName;
+
+///  End  ///
+
+///  Start  ///
 /// A string can be converted to an array with the split() method:
 
 let contentTextTwo = "Hello";
 const arrSplit = contentTextTwo.split("");
-// console.log(arrSplit);
-
-loopItem = " ";
+console.log(contentTextTwo);
+contentTextTwo = "";
 for (let i = 0; i < arrSplit.length; i++) {
-  loopItem += arrSplit[i] + "<br>";
-  // console.log(loopItem);
+  // console.log(i);
+  // console.log(contentTextTwo);
+  contentTextTwo += arrSplit[i] + "<br>";
 }
-console.log(loopItem);
-var countSplit = document.getElementsByClassName("display-split");
-countSplit.innerHTML = loopItem;
+// console.log(contentTextTwo);
+document.getElementById("displaySplit").innerHTML = contentTextTwo;
 
 ///  End  ///
+
 ///  Start  ///
 /// The length property returns the length of a string
 var contentText = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
