a0735b32ee591abfc867070ca68fa4e4fa0e6305
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-02-25 23:41:23 +0600

    add: learning indexOf

---
+++ string/js/string-search.js
@@ -0,0 +1,6 @@
+///  Start  ///
+/// The indexOf() method returns the index (position) of the first occurrence of a string in a string, or it returns -1 if the string is not found:
+/// JavaScript counts positions from zero. 0 is the first position in a string, 1 is the second, 2 is the third, ...
+let text = "My name is Mosfequr Rahamn";
+let varIndexOf = text.indexOf("ahamn");
+document.getElementById("displayIndexOf").innerHTML = varIndexOf;
