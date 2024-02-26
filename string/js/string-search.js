ce39e29d44fad8cd19df8e8b06dea380fa17ec3f
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-02-26 22:16:57 +0600

    add: learning search() method

--- string/js/string-search.js
+++ string/js/string-search.js
@@ -1,6 +1,15 @@
 ///  Start  ///
+/// The search() method searches a string for a string (or a regular expression) and returns the position of the match:
+var textSearch = "I am an Engineer";
+// let varSearch = textSearch.search(Eng);
+let varSearch = textSearch.search("Eng");
+document.getElementById("displaySearch").innerHTML = varSearch;
+///  end  ///
+
+///  Start  ///
 /// The indexOf() method returns the index (position) of the first occurrence of a string in a string, or it returns -1 if the string is not found:
 /// JavaScript counts positions from zero. 0 is the first position in a string, 1 is the second, 2 is the third, ...
 let text = "My name is Mosfequr Rahamn";
 let varIndexOf = text.indexOf("ahamn");
 document.getElementById("displayIndexOf").innerHTML = varIndexOf;
+///  end  ///
