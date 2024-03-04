ba243f31cad27803e821a9ceaafae5ad6c0c27e7
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-03-04 23:50:56 +0600

    add: start learning array method length (array)

---
+++ string/js/array-methods.js
@@ -0,0 +1,13 @@
+///  Start  ///
+///
+
+///  End  ///
+
+///  Start  ///
+/// The length property returns the length (size) of an array:
+
+const myNameLength = ["My", "Name", "is", "Mosfequr", "Rahman"];
+let arrayLength = myNameLength.length;
+document.getElementById("display-length").innerHTML = arrayLength;
+
+///  End  ///
