10b579907fd44978ab9ed69fe79184a94f7ddc16
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-03-05 23:48:16 +0600

    add: implements toString() and at() (array method)

--- string/js/array-methods.js
+++ string/js/array-methods.js
@@ -1,6 +1,17 @@
 ///  Start  ///
-///
+/// The at() method returns an indexed element from an array.
+const varAt = ["My", "Name", "is", "Mosfequr", "Rahman"];
+let tempAt = varAt.at(2);
+document.getElementById("display-at").innerHTML = tempAt;
 
+///  End  ///
+
+///  Start  ///
+/// The JavaScript method toString() converts an array to a string of (comma separated) array values.
+
+var vartoString = ["My", "Name", "is", "Mosfequr", "Rahman"];
+let temptoString = vartoString.toString();
+document.getElementById("display-to-string").innerHTML = temptoString;
 ///  End  ///
 
 ///  Start  ///
