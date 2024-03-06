fa7ea0d0f4a9c866e0970725dc70cb8060722761
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-03-06 23:25:47 +0600

    add: join,push,pop (array (method)

--- string/js/array-methods.js
+++ string/js/array-methods.js
@@ -1,4 +1,32 @@
 ///  Start  ///
+///
+///  End  ///
+
+///  Start  ///
+///
+///  End  ///
+
+///  Start  ///
+///This is what popping and pushing is: Popping items out of an array, or pushing items into an array.
+const varPush = ["My", "Name", "is", "Mosfequr", "Rahman", "Engineering"];
+let tempPush = varPush.push("Engineering");
+document.getElementById("display-push").innerHTML = tempPush;
+console.log(varPush);
+const varPop = ["My", "Name", "is", "Mosfequr", "Rahman", "Engineering"];
+let tempPop = varPush.pop();
+document.getElementById("display-pop").innerHTML = tempPop;
+console.log(tempPop);
+///  End  ///
+
+///  Start  ///
+/// The join() method also joins all array elements into a string.
+/// It behaves just like toString(), but in addition you can specify the separator:
+const varJoin = ["My", "Name", "is", "Mosfequr", "Rahman"];
+let tempJoin = varJoin.join(" * ");
+document.getElementById("display-join").innerHTML = tempJoin;
+///  End  ///
+
+///  Start  ///
 /// The at() method returns an indexed element from an array.
 const varAt = ["My", "Name", "is", "Mosfequr", "Rahman"];
 let tempAt = varAt.at(2);
