8ed95f7cfb763406bb12790381c7b830bc7c31cc
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-03-14 23:46:51 +0600

    add: start learning array

---
+++ string/js/js-array.js
@@ -0,0 +1,4 @@
+// An array can hold many values under a single name, and you can access the values by referring to an index number.
+// Spaces and line breaks are not important. A declaration can span multiple lines:
+const info = ["Mosfequr", "Rahman", "Engineer"];
+console.log(info);
