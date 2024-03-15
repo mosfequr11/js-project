ee5f261cfd4d1b605aefabde24ea7395e8516e13
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-03-15 23:40:41 +0600

    add: learning array

--- string/js/js-array.js
+++ string/js/js-array.js
@@ -1,3 +1,16 @@
+/// start ///
+
+/// End ///
+
+/// start ///
+/// You can also create an array, and then provide the elements:
+const infoarr = [];
+infoarr[0] = "Mosfequr";
+infoarr[1] = "Rahman";
+infoarr[2] = "Engineer";
+document.getElementById("arrDisplay").innerHTML = infoarr;
+/// End ///
+
 // An array can hold many values under a single name, and you can access the values by referring to an index number.
 // Spaces and line breaks are not important. A declaration can span multiple lines:
 const info = ["Mosfequr", "Rahman", "Engineer"];
