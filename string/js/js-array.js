e6ab3b71001aa65a41d2b0447bb82b5f5a4ba8b4
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-03-16 23:56:52 +0600

    add: Accessing array and creating array

--- string/js/js-array.js
+++ string/js/js-array.js
@@ -3,6 +3,21 @@
 /// End ///
 
 /// start ///
+/// Accessing Array Elements
+const varAccessArry = ["Mosfequr", "Rahman", "Engineer"];
+let tempAccessArry = varAccessArry[0];
+let tempAccessArry2 = varAccessArry[1];
+console.log(tempAccessArry);
+console.log(tempAccessArry2);
+/// End ///
+
+/// start ///
+/// the JavaScript Keyword new
+const arrInfo = new Array("Mosfequr", "Rahman", "Engineer");
+document.getElementById("arrInfoDisplay").innerHTML = arrInfo;
+/// End ///
+
+/// start ///
 /// You can also create an array, and then provide the elements:
 const infoarr = [];
 infoarr[0] = "Mosfequr";
@@ -14,4 +29,5 @@ document.getElementById("arrDisplay").innerHTML = infoarr;
 // An array can hold many values under a single name, and you can access the values by referring to an index number.
 // Spaces and line breaks are not important. A declaration can span multiple lines:
 const info = ["Mosfequr", "Rahman", "Engineer"];
-console.log(info);
+// console.log(info);
+document.getElementById("infoDisplay").innerHTML = info;
